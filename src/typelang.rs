0bd7bf623206e0817471ae5825220d049c6647fd
Martin Billinger-Finke <m.billinger-finke@daphos.ai>
2022-09-22 11:40:03 +0200

    one local variable

--- src/typelang.rs
+++ src/typelang.rs
@@ -1,7 +1,6 @@
 use std::collections::HashMap;
 use std::hash::Hash;
 use crate::vm::{Int, Op, RecordSignature, R, Half};
-use crate::vm::Op::GetAddr;
 
 #[derive(Debug, Clone)]
 pub enum Type {
@@ -87,6 +86,7 @@ impl AstNode for FunctionDefinition {
 
         let mut local_env = env.clone();
         for (p, t) in &self.params {
+            // todo don't bind all parameters to local 0
             local_env = assoc(p.clone(), (Binding::Local(0), t.clone()), &local_env);
         }
 
@@ -95,7 +95,6 @@ impl AstNode for FunctionDefinition {
             code.push(Op::Copy(R::Arg, R::Lcl));
         }
         code.extend(self.body.compile(&local_env));
-        code.push(Op::Halt);
         code
     }
 }
@@ -103,7 +102,7 @@ impl AstNode for FunctionDefinition {
 impl ToplevelDefinition for RecordDefinition {}
 
 impl AstNode for RecordDefinition {
-    fn compile(&self, env: &Env) -> Vec<Op<String>> {
+    fn compile(&self, _env: &Env) -> Vec<Op<String>> {
         let mut n_primitive = 0;
         let mut n_pointer = 0;
 
@@ -128,13 +127,13 @@ impl AstNode for RecordDefinition {
 impl Expression for Constant {}
 
 impl AstNode for Constant {
-    fn compile(&self, env: &Env) -> Vec<Op<String>> {
+    fn compile(&self, _env: &Env) -> Vec<Op<String>> {
         vec![Op::Const(self.0)]
     }
 }
 
 impl Typed for Constant {
-    fn type_<'a>(&self, env: &'a Env) -> &'a Type {
+    fn type_<'a>(&self, _env: &'a Env) -> &'a Type {
         &Type::Integer
     }
 }
@@ -151,7 +150,7 @@ impl AstNode for Reference {
     fn compile(&self, env: &Env) -> Vec<Op<String>> {
         match env[&self.0] {
             (Binding::Static, _) => vec![Op::GetAddr(self.0.clone())],
-            (Binding::Local(idx), _) => todo!(),
+            (Binding::Local(idx), _) => vec![Op::GetVal(R::Lcl, idx)],
         }
     }
 }
@@ -207,7 +206,7 @@ impl AstNode for FunCall {
 }
 
 impl Typed for FunCall {
-    fn type_<'a>(&self, env: &'a Env) -> &'a Type {
+    fn type_<'a>(&self, _env: &'a Env) -> &'a Type {
         todo!()
     }
 }
@@ -237,7 +236,7 @@ impl AstNode for ContCall {
 }
 
 impl Typed for ContCall {
-    fn type_<'a>(&self, env: &'a Env) -> &'a Type {
+    fn type_<'a>(&self, _env: &'a Env) -> &'a Type {
         todo!()
     }
 }
@@ -260,21 +259,21 @@ mod tests {
                 /*Box::new(RecordDefinition {
                     name: "Data".to_string(),
                     fields: vec![("x".to_string(), Type::Integer)],
-                }),
+                }),*/
                 Box::new(FunctionDefinition {
                     name: "foo".to_string(),
                     params: vec![("k".to_string(), Type::Continuation)],
-                    body: Box::new(FunCall {
+                    body: Box::new(ContCall {
                         name: "k".to_string(),
-                        args: vec![Box::new(Constant(42))],
+                        val: Box::new(Constant(42)),
                     }),
-                }),*/
+                }),
                 Box::new(FunctionDefinition {
                     name: "main".to_string(),
                     params: vec![("k".to_string(), Type::Continuation)],
-                    body: Box::new(ContCall {
-                        name: "k".to_string(),
-                        val: Box::new(Constant(42))
+                    body: Box::new(FunCall {
+                        name: "foo".to_string(),
+                        args: vec![Box::new(Reference::new("k"))]
                     }),
                 }),
             ],
@@ -289,6 +288,7 @@ mod tests {
 
         let mut vm = Vm::default();
         let res = vm.run(&code);
-        panic!("{res}");
+
+        assert_eq!(res, 42);
     }
 }
